31ca1f8427eb6407209e6360f8e1f94592e843d1
Mtsewu <sirmayayamtsewu@gmail.com>
2022-07-05 15:54:04 +0200

    donefornow

---
+++ score.js
@@ -0,0 +1,50 @@
+let results =document.getElementById("hscore")
+
+let count = 0
+
+function add1()
+{
+    count+=1
+    results.textContent=count
+    
+    
+}
+
+function add2()
+{
+    count+=2
+    
+    results.textContent=count
+}
+
+function add3()
+{
+    count+=3
+
+    results.textContent=count
+}
+
+
+let outcome= document.getElementById("Ascore")
+let x=0
+
+function add1A()
+{
+    x+=1
+    outcome.textContent=x
+    
+    
+}
+
+function add2A()
+{
+    x+=2
+
+    outcome.textContent=x
+}
+
+function add3A()
+{
+    x+=3
+    outcome.textContent=x
+}
