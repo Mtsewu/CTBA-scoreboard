2faa0dfc38cdf9aac02d1c1ccf6344daa8885deb
Mtsewu <sirmayayamtsewu@gmail.com>
2022-07-25 16:43:27 +0200

    nowkinda has timer

--- score.js
+++ score.js
@@ -1,4 +1,30 @@
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+      minutes = parseInt(timer / 60, 10);
+      seconds = parseInt(timer % 60, 10);
+
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+
+      display.textContent = minutes + ":" + seconds;
+
+      if (--timer < 0) {
+        timer = duration;
+      }
+    }, 1000);
+  }
+
+  window.onload = function () {
+    var twelveMinutes = 60 * 12,
+    display = document.querySelector('#time');
+    startTimer(twelveMinutes, display);
+  };
+
+
+
 let results =document.getElementById("hscore")
+
 
 let count = 0
 
