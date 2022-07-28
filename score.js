ac9aa31cfc64a898cb466c0b6ac7db204489167b
Mtsewu <sirmayayamtsewu@gmail.com>
2022-07-29 00:32:56 +0200

    added time with pause and start button

--- score.js
+++ score.js
@@ -1,27 +1,60 @@
+// 12 minutes from now
+var time_in_minutes = 12;
+var current_time = Date.parse(new Date());
+var deadline = new Date(current_time + time_in_minutes*60*1000);
 
 
-function startTimer(duration, display) {
-    var timer = duration, minutes, seconds;
-    setInterval(function () {
-      minutes = parseInt(timer / 60, 10);
-      seconds = parseInt(timer % 60, 10);
+function time_remaining(endtime){
+	var t = Date.parse(endtime) - Date.parse(new Date());
+	var seconds = Math.floor( (t/1000) % 60 );
+	var minutes = Math.floor( (t/1000/60) % 60 );
+	var hours = Math.floor( (t/(1000*60*60)) % 24 );
+	var days = Math.floor( t/(1000*60*60*24) );
+	return {'total':t, 'days':days, 'hours':hours, 'minutes':minutes, 'seconds':seconds};
+}
 
-      minutes = minutes < 10 ? "0" + minutes : minutes;
-      seconds = seconds < 10 ? "0" + seconds : seconds;
+var timeinterval;
+function run_clock(id,endtime){
+	var clock = document.getElementById(id);
+	function update_clock(){
+		var t = time_remaining(endtime);
+		clock.innerHTML = 'minutes: '+t.minutes+'<br>seconds: '+t.seconds;
+		if(t.total<=0){ clearInterval(timeinterval); }
+	}
+	update_clock(); // run function once at first to avoid delay
+	timeinterval = setInterval(update_clock,1000);
+}
+run_clock('clockdiv',deadline);
 
-      display.textContent = minutes + ":" + seconds;
 
-      if (--timer < 0) {
-        timer = duration;
-      }
-    }, 1000);
-  }
+var paused = false; // is the clock paused?
+var time_left; // time left on the clock when paused
 
-  window.onload = function () {
-    var twelveMinutes = 60 * 12,
-    display = document.querySelector('#time');
-    startTimer(twelveMinutes, display);
-  };
+function pause_clock(){
+	if(!paused){
+		paused = true;
+		clearInterval(timeinterval); // stop the clock
+		time_left = time_remaining(deadline).total; // preserve remaining time
+	}
+}
+
+function resume_clock(){
+	if(paused){
+		paused = false;
+
+		// update the deadline to preserve the amount of time remaining
+		deadline = new Date(Date.parse(new Date()) + time_left);
+
+		// start the clock
+		run_clock('clockdiv',deadline);
+	}
+}
+
+// handle pause and resume button clicks
+document.getElementById('pause').onclick = pause_clock;
+document.getElementById('start').onclick = resume_clock;
+
+
 
 
 
